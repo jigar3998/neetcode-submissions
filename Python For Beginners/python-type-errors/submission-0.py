16f512a3101115257ae22eb6e3013d8e926fdbce
Jigar Mehta <48583746+jigar3998@users.noreply.github.com>
2026-09-21 22:38:12 -0400

    Add: python-type-errors - submission-0

---
+++ Python For Beginners/python-type-errors/submission-0.py
@@ -0,0 +1,14 @@
+
+# Don't modify the code below this line
+variable = 10
+
+print(variable)
+
+variable = "hello"
+print(type(variable))
+
+variable = 10
+variable = int(variable)
+
+print(variable)
+print(type(variable))
